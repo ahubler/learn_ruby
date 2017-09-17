71600be345031cd04d65e6200941a281d286c704
Andrew Hubler <hubler.andrew@gmail.com>
2017-09-16 20:30:57 -0400

    Completed ruby code for exercise 1 and two. Completed tests the exercise 2s spec file.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(fahrenheit_temperature)
+	celcius_temperature = (fahrenheit_temperature -32)* 5.0/9.0
+end
+
+def ctof(celcius_temperature)
+	fahrenheit_temperature = (celcius_temperature / (5.0/9.0))+32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
 #write your code here
+def add(first_number, second_number)#Simple addition
+	first_number + second_number
+end
+
+def subtract(first_number, second_number)#Simple subtraction
+	first_number - second_number
+end
+
+def sum(number_array)#Sums all the values in an array
+	if number_array.length > 0#if the array is not empty
+		total = 0
+		number_array.each {|i| total += i}#add the value of each element to the total
+		total#retun the total
+	else
+		total = 0#if the array is empty
+	end
+end
+
+def multiply(*p)#Multiplies any number of arguments
+	if p.length > 0
+		product = 1
+		p.each{|i| product *= i}#multiplies al the elements together
+		product
+	else
+		product = 0
+	end
+end
+
+def power(number, exponent)#calculates number^exponent
+	number ** exponent
+end
+
+def factorial(number)#calculates n! of number
+	if number > 1 #n! of 0 and 1 is just 1, so we don't need to calculate that
+		i = 1
+		number_factorial = 1
+		while i <= number #i will iterate through all positive integers less than or equal to number
+			number_factorial *= i
+			i += 1
+		end
+		number_factorial
+	else 
+		number_factorial = 1	
+	end
+end
